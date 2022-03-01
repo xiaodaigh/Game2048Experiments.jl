2eedaeb352d968be84c3c32b54eb7f32dd95546f
ZJ Dai <zhuojia.dai@gmail.com>
2022-03-01 22:41:54 +1100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.26

---
+++ src/Game2048Experiments.jl
@@ -0,0 +1,5 @@
+module Game2048Experiments
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using Game2048Experiments
+using Test
+
+@testset "Game2048Experiments.jl" begin
+    # Write your tests here.
+end
